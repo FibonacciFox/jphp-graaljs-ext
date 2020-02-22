4e872b2595648c17e7bb3bf354abd76bf283f480
Max <61143434+FibonacciFox@users.noreply.github.com>
2020-02-22 09:22:28 +0300

    update

--- sdk/php/graaljs/Context.php
+++ sdk/php/graaljs/Context.php
@@ -18,25 +18,13 @@ namespace php\graaljs;
  * <p> — абзац;
  * <pre> — форматированный текст;
  * <samp> — пример;
- * <ul> — маркированный список;
- * <var> — имя переменной.
- * Инлайн тег. Использует данные с {@link http://кодер.укр/data.php}
- * Далее будет список используемых тегов
- *
- * @author Coder UA <web@кодер.укр>
- * @version 1.0
- * @package MyPackageName
- * @category MyCategoryNews
- * @todo описание необходимых доработок
- * @example http://кодер.укр/example.php
- * @copyright Copyright (c) 2014, Coder UA
  */
 abstract class Context
 {
     /**
      * @param string $permittedLanguages
      * 
-     * @return Context
+     * @return Context {}
      */
     public function __construct(string $permittedLanguages = null)
     {
